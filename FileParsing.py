c24afe393ee16e55731b5caae4d3042dae1e9a4b
Sara Ann Abdilla <saraabdilla274@gmail.com>
2016-10-13 13:45:37 +0200

    Update to Python v2.7

--- FileParsing.py
+++ FileParsing.py
@@ -9,19 +9,19 @@ import pandas
 
 #To efficiently read the genome:
 def readGenome(filename):
-    filehandle = open(filename, 'r', encoding="latin1")
+    filehandle = open(filename, 'r')#, encoding="latin1")
     #ignore boolean (x[0]) and hold header or sequence since they alternate
     iteration = (x[1] for x in groupby(filehandle, lambda line: line[0] == ">"))
     for header in iteration:
-        header.__next__()[1:].strip() #drop '>'
-        seq = "".join(s.strip() for s in iteration.__next__()) #join all sequence lines
-        yield seq
-
+        header.next()[1:].strip() #drop '>'
+        seq = "".join(s.strip() for s in iteration.next()) #join all sequence lines
+        yield seq        
+        
 #To read the genome using SeqIO:
 def readGenomeSeqIO(filename):
     genome = ''   
     #rU - open file for reading in universal readline mode (works across platforms due to differing newline characters)    
-    filehandle = open(filename, 'rU', encoding="latin1")
+    filehandle = open(filename, 'rU')#, encoding="latin1")
     for record in SeqIO.parse(filehandle, "fasta"): #SeqRecord iterator - multiple records
     #record = SeqIO.read(filehandle, "fasta") #one record
         genome += record.seq #parses records one by one, without changing the file order
@@ -31,7 +31,7 @@ def readGenomeSeqIO(filename):
 #To inefficiently read the genome:
 def readGenomeInefficient(filename):
     genome = '' 
-    with open(filename, 'r', encoding="latin1") as file: #opening a file for reading
+    with open(filename, 'r') as file: #opening a file for reading
         for line in file:
             if line[0] != '>': #ignore header line with genome information
                 genome += line.rstrip() #add each line of bases to the string 
@@ -42,7 +42,7 @@ def readGenomeInefficient(filename):
 def readSequence(filename):
     #filehandle = open(filename, 'rU', encoding="latin1")
     #tsvreader = csv.reader(filehandle, delimiter="\t")
-    with open(filename, encoding="latin1") as file:     
+    with open(filename) as file:     
         while True: #loops every 4 lines (each read is a set of 4) indefinitely until EOF
             next(file) #skip tag line 
             seq = file.readline().rstrip() #string of DNA bases
@@ -55,7 +55,7 @@ def readSequence(filename):
 #To inefficiently read the sequencing reads:          
 def readSequenceInefficient(filename):
     sequences = []
-    with open(filename, encoding="latin1") as file:  
+    with open(filename) as file:  
         while True: #loops every 4 lines (each read is a set of 4) indefinitely until EOF
             file.readline() #read tag
             seq = file.readline().rstrip() #string of DNA bases
--- Main.py
+++ Main.py
@@ -7,6 +7,7 @@ import Alignment
 import Visualisation
 
 from path import path
+import collections
 import pdb
 
 #pdb.set_trace()
@@ -15,17 +16,17 @@ import pdb
 genome = FileParsing.readGenomeInefficient(path('Data\PhixGenome.fa').abspath())
 reads = FileParsing.readSequenceInefficient(path('Data\PhiXSequencingReads1000.fastq').abspath())
 
-#print("Length of the genome: ", len(genome))
-#print("Frequency of each base in the genome: ", collections.Counter(genome))
+print "Length of the genome: %d" % len(genome)
+print "Frequency of each base in the genome:", collections.Counter(genome)
 
-#print("Length of the reads: ", len(reads))
-#readsFreq = collections.Counter()
-#for read in reads:
-    #readsFreq.update(read)
-#print("Frequency of each base in the reads: ", readsFreq)
+print "Length of the reads: %d" % len(reads)
+readsFreq = collections.Counter()
+for read in reads:
+    readsFreq.update(read)
+print "Frequency of each base in the reads:", readsFreq
 
 matches, count, offsets = Alignment.align(reads, genome)
-print(matches, "/", count, " reads matched the genome")
+print "%d/%d reads matched the genome." % (matches, count)
 #The result is not 100% but this is to be expected due to sequencing errors. 
     
 textFile = path('Output Test Files\DataVisualisationTest.txt').abspath()
