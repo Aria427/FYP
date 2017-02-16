a6a2e6f5e4f0d818c488489bcfd4275a46d64c18
Sara Ann Abdilla <saraabdilla274@gmail.com>
2017-02-16 14:17:02 +0100

    Slight modification to analysis visual

--- genomeCompressionComparison.py
+++ genomeCompressionComparison.py
@@ -5,17 +5,17 @@ import matplotlib.pyplot as plt
 
 #This function generates a histogram of the different human genome sizes, before and after compression.
 def compressionComparison(output):
-    unzippedSize = 3273481150
-    gzippedSize = 983659424
-    intSize = 975782472  #fileParsing.parseGenomeInt() 
-    longSize = 975782472  #fileParsing.parseGenomeLong()
+    unzippedSize = 3040 #3273481150
+    gzippedSize = 938 #983659424
+    intSize = 930 #975782472  #fileParsing.parseGenomeInt() 
+    longSize = 930 #975782472  #fileParsing.parseGenomeLong()
     
     dictionary = {u'Unzipped': unzippedSize, u'G-zipped': gzippedSize,  \
                   u'Int Compression': intSize, u'Long Compression': longSize} #key:value
-                  
+    
     plt.bar(range(len(dictionary)), dictionary.values(), align='center', color='purple')
     plt.title('Bar Chart of Human Genome Compression Sizes')
     plt.xticks(range(len(dictionary)), dictionary.keys())
-    plt.ylabel('Size / bytes')
+    plt.ylabel('Size / MB')
     plt.savefig(output)
     plt.show()
