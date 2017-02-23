522206787e7883c0009449c31d89b4922b7fff79
Sara Ann Abdilla <saraabdilla274@gmail.com>
2017-02-23 17:11:27 +0100

    Possible map phase for BWA MapReduce implementation

---
+++ mapperBWA.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+#This file contains the map step for the aligment MapReduce implementation.
+
+import sys
+
+def readInputGenome(file):
+    for line in file:
+        if line and line[0] != '>':
+            yield line.rstrip().upper().replace('N', '').replace(' ','') 
+            
+def readInputReads(file):
+    flag, sequence, quality = '', '', ''
+    while True: #runs until EOF
+        line = file.readline() 
+        if not line: #reached EOF
+            break
+        
+        if line.startswith('#'): #read details
+            line = file.readline()
+            pass
+        
+        elif line.startswith('>'): #>flags reads scores
+            line = file.readline()
+            pass
+        
+        else:
+            line = line.split()
+            flag = line[0]
+            sequence = line[1]
+            quality = line[2]
+            
+            if sequence == 'NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN':
+                line = file.readline()
+                pass
+            
+            else:
+                sequence = sequence.rstrip().upper().replace('N', '').replace(' ', '')
+                yield sequence  
+                    
+def main():
+    #input file comes from STDIN (standard input)
+    genomeSeq = readInputGenome(sys.stdin)
+    readSeq = readInputReads(sys.stdin)
+    
+    overlap = '' 
+    genomeStartIndex = 0
+    for g in genomeSeq:
+        g = overlap + g #append to start of next line to handle patterns found between lines
+
+        #write results to STDOUT (standard output)
+        print '%s\t%d\t%s' % (g, genomeStartIndex, 'G') #tab-delimited
+        
+        overlap = g[-60:] #overlap = length of read
+        genomeStartIndex += 50 #index of each subsequence is incremented by length of genome line 
+        #The output here will be the input for the reduce step.
+      
+    readStartIndex = 0
+    for r in readSeq:
+        #write results to STDOUT (standard output)
+        print '%s\t%d\t%s' % (r, readStartIndex, 'R') #tab-delimited
+        
+        readStartIndex += 60 #index of each read is incremented by length of read
+
+if __name__ == '__main__':
+    main()       
+
