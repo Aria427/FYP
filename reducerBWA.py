a37767dd3e36db9049adea0b0f34c093fdf7662b
Sara Ann Abdilla <saraabdilla274@gmail.com>
2017-02-23 17:46:03 +0100

    Update to reducer phase

--- reducerBWA.py
+++ reducerBWA.py
@@ -1,19 +1,49 @@
 #!/usr/bin/env python
-#This file contains the reduce step for the MapReduce implementation.       
+#This file contains the reduce step for the alignment MapReduce implementation.       
 
+import alignmentString
 import sys
 
-#This function reads in the output from the mapper using a generator.
-def readMapperOutput(file, separator='\t'):
+#This function reads in the output from the combiner using a generator.
+def readCombinerOutput(file, separator='\t'):
     for line in file:
-        yield line.rstrip().split(separator)
+        yield line.rstrip().split(separator, 1)
 
+#This function iterates through the genome lines.
+def genomeLine(data):
+    for isGR, grValue in data:
+        if isGR == 'G':
+            for g in grValue:
+                genSeq = g[0] 
+                yield genSeq   
+        else: 
+            pass
+
+#This function iterates through the read lines.               
+def readLine(data):
+    for isGR, grValue in data:
+        if isGR == 'R':
+            for r in grValue:
+                readSeq = r[1] 
+                yield readSeq  
+        else:
+            pass
+        
 def main():
     #input comes from STDIN (standard input)
-    data = readMapperOutput(sys.stdin) 
+    data = readCombinerOutput(sys.stdin) 
   
-    for isGR, tuple in data:
-        print "%s\t%s" % (isGR, tuple) #write result to STDOUT
+    genome = genomeLine(data)
+    reads = readLine(data)
+    
+    totalMatches, totalCount, totalOffsets = 0, 0, []
+    for g in genome:
+         matchesCount, count, offsets = alignmentString.alignFM(reads, g)
+            
+         totalMatches += matchesCount
+         totalCount = count #number of reads stays the same as every chunk goes through each read again
+         totalOffsets.append(offsets)
+         print '%d/%d reads matched the genome.' % (totalMatches, totalCount) #write result to STDOUT
 
 if __name__ == '__main__':
     main()
