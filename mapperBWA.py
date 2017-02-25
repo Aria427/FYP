ea6b6f5c856784801008542cd3eaf479bf3eefbe
Sara Ann Abdilla <saraabdilla274@gmail.com>
2017-02-25 18:31:24 +0100

    Possible adaptation of combined mapper

--- mapperBWA.py
+++ mapperBWA.py
@@ -77,32 +77,8 @@ def readInputPhiXReads(file):
                     line = file.readline()
                   
 def main():
-    #input file comes from STDIN (standard input)
-    if sys.stdin.name.endswith('fastq'):#'.bz2'):
-        readSeq = readInputPhiXReads(sys.stdin)
-        readStartIndex = 0
-        
-        for r in readSeq:
-            #write results to STDOUT (standard output)
-            print '%s\t%s' % ('R', (r, readStartIndex)) #tab-delimited key:value
-            #key = character identifying read
-            #value = tuple with sequence and its start position
-            
-            readStartIndex += 100 #index of each read is incremented by length of read
-    
-    elif sys.stdin.name.endswith('.bz2'):
-        readSeq = readInputReads(sys.stdin)
-        readStartIndex = 0
-        
-        for r in readSeq:
-            #write results to STDOUT (standard output)
-            print '%s\t%s' % ('R', (r, readStartIndex)) #tab-delimited key:value
-            #key = character identifying read
-            #value = tuple with sequence and its start position
-            
-            readStartIndex += 60 #index of each read is incremented by length of read
-                   
-    else:
+    #input files comes from STDIN (standard input)
+    if sys.stdin.readline().startswith('>'): #if the first line of the input starts with '>' => genome header information
         genomeSeq = readInputGenome(sys.stdin) #reads genome file as well as simple text  
         overlap = '' 
         genomeStartIndex = 0
@@ -114,11 +90,39 @@ def main():
             print '%s\t%s' % ('G', (g, genomeStartIndex)) #tab-delimited key:value
             #key = character identifying genome
             #value = tuple with sequence and its start position
+            #The output here will be the input for the reduce step.
             
-            overlap = g[-60:] #overlap = length of read
+            overlap = g[-100:] #overlap = length of read
             genomeStartIndex += 50 #index of each subsequence is incremented by length of genome line 
-            #The output here will be the input for the reduce step.    
+      
+    elif sys.stdin.readline().startswith('#'): #if the first line of the input starts with '#' => human sequencing reads
+        readSeq = readInputReads(sys.stdin)
+        readStartIndex = 0
+        
+        for r in readSeq:
+            #write results to STDOUT (standard output)
+            print '%s\t%s' % ('R', (r, readStartIndex)) #tab-delimited key:value
+            #key = character identifying read
+            #value = tuple with sequence and its start position
+	    #The output here will be the input for the reduce step.
+            
+            readStartIndex += 60 #index of each read is incremented by length of read
+      
+    else: #if the first line of the input starts with '@' => PhiX sequencing reads
+        readSeq = readInputPhiXReads(sys.stdin)
+        readStartIndex = 0
+        
+        for r in readSeq:
+            #write results to STDOUT (standard output)
+            print '%s\t%s' % ('R', (r, readStartIndex)) #tab-delimited key:value
+            #key = character identifying read
+            #value = tuple with sequence and its start position
+            #The output here will be the input for the reduce step.
+            
+            readStartIndex += 100 #index of each read is incremented by length of read           
 
 if __name__ == '__main__':
     main()       
 
+#Run MapReduce job on Hadoop using:
+#   bin/hadoop jar share/hadoop/tools/lib/hadoop-streaming-2.7.3.jar -file /home/hduser/mapperBWA.py -mapper /home/hduser/mapperBWA.py -file /home/hduser/reducerBWA.py -reducer /home/hduser/reducerBWA.py -file /home/hduser/alignmentString.py -file /home/hduser/matchingString.py -input /user/hduser/PhiXGenome.fa -input /user/hduser/PhiXSequencingReads1000.fastq -output /user/hduser/bwaPhiX-output
