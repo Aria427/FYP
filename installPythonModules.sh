e474966a43c09a33d18f3eaed61731edbfcbff86
Sara Ann Abdilla <saraabdilla274@gmail.com>
2017-04-04 14:52:55 +0200

    Amended bootstrap action

--- installPythonModules.sh
+++ installPythonModules.sh
@@ -5,6 +5,7 @@ sudo rm /usr/bin/python
 sudo ln -s /usr/bin/python2.7 /usr/bin/python
 
 sudo -H pip install ipython[all]
-
-sudo -H pip install smart_open
 sudo -H pip install requests[security]
+
+export AWS_ACCESS_KEY_ID=AKIAJLG2HIPH47IWGK4A
+export AWS_SECRET_ACCESS_KEY=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
